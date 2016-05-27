9be84d7f437a8f40f551ba05996d8cd518f2b0aa
73mp74710n <zombieleetnca@gmail.com>
2016-05-27 02:45:52 +0100

    Update function.bash

--- function.bash
+++ function.bash
@@ -17,6 +17,8 @@ substr() {
     # + if an error occured substr_usage will be called
     # + on error return status 1
     # + usage: substr 1 2 "bash" >>>> as
+
+    
     local start=$1 end=$2
     local str=$3
     local substrResult;
@@ -48,6 +50,8 @@ inArray() {
     # + if match is not found the return status 2
     # + array=( csh zsh tcsh bash dash ksh )
     # + usage: inArray "bash"  "${array[@]}" >>>> "bash"
+
+    
     local value=$1
     declare -a inArrayResult;
     inArray_usage() {
@@ -72,6 +76,8 @@ len() {
     # + get the length of a value
     # + where value is the string, number or array to get it's length
     # + usage: len "bash" >>>> "4"
+
+    
     local value=$1
     len_usage() {
 	setColor fg red
@@ -92,6 +98,8 @@ lenOfArray() {
     # + get the length of an array
     # + where array is the array to get it's length
     # + usage: len "bash" >>>> "4"
+
+    
     local array=$1
     lenOfArray_usage() {
 	setColor fg red
@@ -113,7 +121,8 @@ rev_chars() {
     # + reverse revChars
     # + where revChars is either an array, string or number
     # + usage: rev_chars "bash" >>>> "hsab"
-    # OSCON
+
+    
     local revChars;
     rev_charsUsage() {
 	setColor fg red
@@ -141,6 +150,8 @@ indexOf() {
     # + where indexOfValue is the string to get its location
     # + str is the string to search for indexOfValue
     # + usage: indexOf 2 "bash" >>>> "a"
+
+    
     local indexOfValue=$1
     local str=$2
     local printFirstCharacter;
@@ -155,7 +166,7 @@ indexOf() {
     while (( ${#str} ));do
 	i+=1;
 	printFirstCharacter=$( printf "%c" "$str" )
-	[[ ${printFirstCharacter} == ${indexOfValue} ]] && \
+	[[ ${printFirstCharacter} == "${indexOfValue}" ]] && \
 	    log "${i}" \
 	    && return 0;
 	str=${str#*?}
@@ -194,7 +205,9 @@ setProxy() {
     # + username and password is the login information of the proxy
     # + username and password is optional
     # + on error return status is 1
-    # + usage: setProxy http 127.0.0.1 8080 user pass 
+    # + usage: setProxy http 127.0.0.1 8080 user pass
+    
+
     local protocol="$1" host="$2"  port="$3"  username="$4" password="$5"
     setProxy_usage() {
 	setColor fg red
@@ -214,7 +227,7 @@ setProxy() {
 		for j in $( awk -F" " '{ print $1}' /etc/services);do
 		    
 		    [[ $j =~ ^[0-9A-Za-z] ]] || continue
-		    [[ $j == ${protocol} ]] && check=true && break || { check=false ;}
+		    [[ $j == "${protocol}" ]] && check=true && break || { check=false ;}
 		
 		done
 		unset j
@@ -260,8 +273,8 @@ setProxy() {
 	    
 	    ########IF USERNAME IS NULL OR UNSET SET THE VALUE OF NULL TO IT
 	    ########IF PASSWORD IS NULL OR UNSET SET THE VALUE OF NULL TO IT
-	    : ${username:=$(${null})}
-	    : ${password:=$(${null})}
+	    uname=${username:=$(${null})}
+	    pass=${password:=$(${null})}
 	    ########IF PASSWORD IS SET BUT USERNAME IS NULL RETURN FROM THIS FUNCTION
 	    [ "${password}" != "${null}" ] && [ "${username}" == "${null}" ] && {
 		setColor fg yellow
@@ -280,8 +293,10 @@ setColor() {
     # + where setColorType can either be fg for foreground or bg for background
     # + usage: setColorType fg red
     # + setColorType fg reset ... resets the color of a text back to default
+
+    
     local setColorType="$1" color="$2"
-    if [ "$(len $@)" != "2" ];then
+    if [ "${#@}" != "2" ];then
 	setColor fg red
 	log "Insufficient Number of argument"
 	setColor fg reset
@@ -383,6 +398,8 @@ toUpperCase() {
     # + on error return status is 1
     # + if value to convert to uppercase is an integer or an uppercase value, return status is 2
     # + usage: toUpperCase "bash" >> "BASH"
+
+    
     local toUpper="${1}"
     toUpperUsage() {
 	setColor fg red
@@ -418,6 +435,8 @@ toLowerCase() {
     # + on error return status is 1
     # + if value to convert to lowercase is an integer or a lowercase value, return status is 2
     # + usage: toLowerCase "BASH" >> "bash"
+
+    
     local toLower="${1}"
     toLowerUsage() {
 	setColor fg red
@@ -479,11 +498,11 @@ split() {
     log "${newArray[@]}"
    return 0;
 }
-<<'EOF'
+
 strict() {
     set -o nounset
     sourceScript=$0
-    checkVariable=$(<${sourceScript})
+    checkVariable=$(<"${sourceScript}")
     OLDIFS=$IFS
     IFS=
     declare -i i=0
@@ -505,12 +524,11 @@ strict() {
 	fi
 	
 	
-    done < <( echo $checkVariable )
+    done < <( echo "$checkVariable" )
     
     IFS="${OLDIFS}"
     return 0;
 }
-EOF
 <<'EOF'
 include() {
     local includeFile=$1
@@ -554,21 +572,24 @@ match() {
 sqrt() {
     
     local numToGetSqrt=$1
+    local finalConversion;
     sqrtUsage() {
 	setColor fg red
 	log "usage:- sqrt numToGetSqrt"
     }
-    if [[ $numToGetSqrt =~ [0-9] ]];then
-	sqrtOfValue=$(( ${numToGetSqrt} / 2 ))
-	log "${sqrtOfValue}";
+    if [[ $numToGetSqrt == +([0-9]) ]];then
+	awk -v numToGetSqrt="$numToGetSqrt" ' BEGIN { print sqrt(numToGetSqrt) }';
+	return 0;
     else
 	sqrtUsage;
-	exit 1;
+	return 1;
     fi
     return 0;
 }
 decimaltobinary() {
     local decimal=$1
+    local finalConversion;
+    local binary;
     decimaltobinaryUsage() {
 	setColor fg red
 	log "usage:- decimaltobinary decimal"
@@ -581,85 +602,105 @@ decimaltobinary() {
     fi
   
     if [ "$decimal" -ge "128" ];then
-	binary=$(( $decimal - 128))
-	printf "%d" "1"
+	binary=$(( decimal - 128))
+	finalConversion+=1
     elif [ "$decimal" -lt "128" ];then 
-	binary=$(( $decimal - 0 ))
-	printf "%d" "0"
+	binary=$(( decimal - 0 ))
+	finalConversion+=0
     fi
     
     if [ "$binary" -ge "64" ];then
-	binary=$(( $binary - 64 ))
-	printf "%d" "1"
+	binary=$(( binary - 64 ))
+	finalConversion+=1
     elif [ "$binary" -lt "64" ];then
-	binary=$(( $binary - 0 ))
-	printf "%d" "0"
+	binary=$(( binary - 0 ))
+	finalConversion+=0
     fi
     
     if [ "$binary" -ge "32" ];then
-	binary=$(( $binary - 32 ))
-	printf "%d" "1"
+	binary=$(( binary - 32 ))
+	finalConversion+=1
     elif [ "$binary" -lt "32" ];then
-	binary=$(( $binary - 0 ))
-	printf "%d" "0"
+	binary=$(( binary - 0 ))
+	finalConversion+=0
     fi
     
     if [ "$binary" -ge "16" ];then
-	binary=$(( $binary - 16 ))
-	printf "%d" "1"
+	binary=$(( binary - 16 ))
+	finalConversion+=1
     elif [ "$binary" -lt "16" ];then
-	binary=$(( $binary - 0 ))
-	printf "%d" "0"
+	binary=$(( binary - 0 ))
+	finalConversion+=0
     fi
     
     if [ "$binary" -ge "8" ];then
-	binary=$(( $binary - 8 ))
-	printf "%d" "1"
+	binary=$(( binary - 8 ))
+	finalConversion+=1
     elif [ "$binary" -lt "8" ];then
-	binary=$(( $binary - 0 ))
-	printf "%d" "0"
+	binary=$(( binary - 0 ))
+	finalConversion+=0
     fi
     
     if [ "$binary" -ge "4" ];then
-	binary=$(( $binary - 4 ))
-	printf "%d" "1"
+	binary=$(( binary - 4 ))
+	finalConversion+=1
     elif [ "$binary" -lt "4" ];then
-	binary=$(( $binary - 0 ))
-	printf "%d" "0"
+	binary=$(( binary - 0 ))
+	finalConversion+=0
     fi
     
     if [ "$binary" -ge "2" ];then
-	binary=$(( $binary - 2 ))
-	printf "%d" "1"
+	binary=$(( binary - 2 ))
+	finalConversion+=1
     elif [ "$binary" -lt "2" ];then
-	binary=$(( $binary - 0 ))
-	printf "%d" "0"
+	binary=$(( binary - 0 ))
+	finalConversion+=0
     fi
     
     if [ "$binary" -ge "1" ];then
-	binary=$(( $binary - 1 ))
-	printf "%d\n" "1"
+	binary=$(( binary - 1 ))
+	finalConversion+=1
     elif [ "$binary" -lt "1" ];then
-	binary=$(( $binary - 0 ))
-	printf "%d\n" "0"
+	binary=$(( binary - 0 ))
+	finalConversion+=0
     fi
+   
+    <<EOF
+ # stripping out leading 0
+    # for example the result of finalConversion for 23 is 0001011
+    # the first three 0's are not needed
+    while [ -n "${finalConversion}" ];do
+	if [[ "$(printf "%c" "$finalConversion" )" == "0" ]];then
+	    finalConversion="${finalConversion#*?}"
+	else
+	    break
+	fi
+	
+    done
+EOF
+    log "$finalConversion"
+    return 0;
 }
 
 reverse() {
-    declare -a array=$1
+    declare -a array;
     declare -a reversedArray;
     reverseUsage() {
 	setColor fg red
 	log "usage:- reverse array" >&2
 	setColor fg reset
     }
-    [ -z "$@" ] && reverseUsage && return 1;
-    [ "${#array[@]}" = "1" ] && reverseUsage && return 1;
+    [[ ! "$@" ]] && reverseUsage && return 1;
+    for arry;do
+	array+=( "${arry}" )
+    done
+    local i;
     for (( i=${#array[@]} ; i >= 0 ; i-- )) {
 	    #log "${array[$i]}"
-	    reversedArray=( "${array[$i]}" )
+	    reversedArray+=( "${array[$i]}" )
 	}
-	log "${reversedArray}"
+	log "${reversedArray[@]}"
+	unset arry
 	return 0;
 }
 
@@ -705,10 +746,10 @@ and() {
 	return 1;
     fi
     
-    numOne=$( decimaltobinary ${numOne} ) numTwo=$( decimaltobinary ${numTwo} )
+    numOne=$( decimaltobinary "${numOne}" ) numTwo=$( decimaltobinary "${numTwo}" )
     
     while [ -n "${numOne}" ] && [ -n "${numTwo}" ];do
-	if [ $( printf "%c" "${numOne}" ) = "1" ] && [ $( printf "%c" "${numTwo}" ) = "1" ];then
+	if [ "$( printf "%c" "${numOne}" )" = "1" ] && [ "$( printf "%c" "${numTwo}" )" = "1" ];then
 	    binaryBitwiseAnd+=1
 	else
 	    binaryBitwiseAnd+=0
@@ -717,7 +758,7 @@ and() {
 	numOne=${numOne#*?} numTwo=${numTwo#*?}
     done
 
-    log $binaryBitwiseAnd
+    echo  "$((2#$binaryBitwiseAnd))"
     return 0;
 }
 
@@ -734,14 +775,14 @@ or() {
 	return 1;
     fi
     
-    numOne=$( decimaltobinary ${numOne} ) numTwo=$( decimaltobinary ${numTwo} )
+    numOne=$( decimaltobinary "${numOne}" ) numTwo=$( decimaltobinary "${numTwo}" )
     
     while [ -n "${numOne}" ] && [ -n "${numTwo}" ];do
-	if [ $( printf "%c" "${numOne}" ) = "1" ] && [ $( printf "%c" "${numTwo}" ) = "1" ];then
+	if [ "$( printf "%c" "${numOne}" )" = "1" ] && [ "$( printf "%c" "${numTwo}" )" = "1" ];then
 	    binaryBitwiseOr+=1
-	elif [ $( printf "%c" "${numOne}" ) = "0" ] && [ $( printf "%c" "${numTwo}" ) = "1" ];then
+	elif [ "$( printf "%c" "${numOne}" )" = "0" ] && [ "$( printf "%c" "${numTwo}" )" = "1" ];then
 	    binaryBitwiseOr+=1
-	elif [ $( printf "%c" "${numOne}" ) = "1" ] && [ $( printf "%c" "${numTwo}" ) = "0" ];then
+	elif [ "$( printf "%c" "${numOne}" )" = "1" ] && [ "$( printf "%c" "${numTwo}" )" = "0" ];then
 	    binaryBitwiseOr+=1
 	else
 	    binaryBitwiseOr+=0
@@ -750,7 +791,7 @@ or() {
 	numOne=${numOne#*?} numTwo=${numTwo#*?}
     done
     
-    log $binaryBitwiseOr
+    echo  "$((2#$binaryBitwiseOr))"
     
     return 0;
 
@@ -770,156 +811,19 @@ xor() {
 	return 1;
     fi
     
-    numOne=$( decimaltobinary ${numOne} ) numTwo=$( decimaltobinary ${numTwo} )
+    numOne=$( decimaltobinary "${numOne}" ) numTwo=$( decimaltobinary "${numTwo}" )
     
      while [ -n "${numOne}" ] && [ -n "${numTwo}" ];do
-	if [ $( printf "%c" "${numOne}" ) = "1" ] && [ $( printf "%c" "${numTwo}" ) = "0" ];then
+	if [ "$( printf "%c" "${numOne}" )" = "1" ] && [ "$( printf "%c" "${numTwo}" )" = "0" ];then
 	    binaryBitwiseXor+=1
-	elif [ $( printf "%c" "${numOne}" ) = "0" ] && [ $( printf "%c" "${numTwo}" ) = "1" ];then
+	elif [ "$( printf "%c" "${numOne}" )" = "0" ] && [ "$( printf "%c" "${numTwo}" )" = "1" ];then
 	    binaryBitwiseXor+=1
 	else
 	    binaryBitwiseXor+=0
 	fi
 	numOne=${numOne#*?} numTwo=${numTwo#*?}
-    done
-
+     done
+     echo  "$((2#$binaryBitwiseXor))"
      return 0;
-}
-
-
-
-onePad() {
-    local stringToCypher=$1
-    local random;
-    local cypheredShifts;
-    local cypher;
-    local cypheredTextNum;
-    local addCharacter;
-    local dateSeconds;
-    local shifts;
-    onePadUsage() {
-	setColor fg red
-	log "usage:- onePad stringToCypher" >&2
-	setColor fg reset
-    }
-    onePadAlphaTest() {
-	local alpha=$1
-	local numAlpha;
-	case $alpha in
-	    A|a) numAlpha=1 ;;
-	    B|b) numAlpha=2 ;;
-	    C|c) numAlpha=3 ;;
-	    D|d) numAlpha=4 ;;
-	    E|e) numAlpha=5 ;;
-	    F|f) numAlpha=6 ;;
-	    G|g) numAlpha=7 ;;
-	    H|h) numAlpha=8 ;;
-	    I|i) numAlpha=9 ;;
-	    J|j) numAlpha=10 ;;
-	    K|k) numAlpha=11 ;;
-	    L|l) numAlpha=12 ;;
-	    M|m) numAlpha=13 ;;
-	    N|n) numAlpha=14 ;;
-	    O|o) numAlpha=15 ;;
-	    P|p) numAlpha=16 ;;
-	    Q|q) numAlpha=17 ;;
-	    R*r) numAlpha=18 ;;
-	    S|s) numAlpha=19 ;;
-	    T|t) numAlpha=20 ;;
-	    U|u) numAlpha=21 ;;
-	    V|v) numAlpha=22 ;;
-	    W|w) numAlpha=23 ;;
-	    X|x) numAlpha=24 ;;
-	    Y|y) numAlpha=25 ;;
-	    Z|z) numAlpha=26 ;;
-	    *) return 1;
-	esac
-	
-	log "${numAlpha}"
-    }
-    oneNumPadNumToAlpha() {
-	local num=$1
-	local numAlpha;
-	case $num in
-	    1) numAlpha=a ;;
-	    2) numAlpha=b ;;
-	    3) numAlpha=c ;;
-	    4) numAlpha=d ;;
-	    5) numAlpha=e ;;
-	    6) numAlpha=f ;;
-	    7) numAlpha=g ;;
-	    8) numAlpha=h ;;
-	    9) numAlpha=i ;;
-	    10) numAlpha=j ;;
-	    11) numAlpha=k ;;
-	    12) numAlpha=l ;;
-	    13) numAlpha=m ;;
-	    14) numAlpha=n ;;
-	    15) numAlpha=o ;;
-	    16) numAlpha=p ;;
-	    17) numAlpha=q ;;
-	    18) numAlpha=r ;;
-	    19) numAlpha=s ;;
-	    20) numAlpha=t ;;
-	    21) numAlpha=u ;;
-	    22) numAlpha=v ;;
-	    23) numAlpha=w ;;
-	    24) numAlpha=x ;;
-	    25) numAlpha=y ;;
-	    26) numAlpha=z ;;
-	    *) return 1;
-	esac
-	
-	log "${numAlpha}"
-    }
-    #ss() {
-    #	local a=$1
-    #   b=$( tr [a-zA-Z] [1-26] <<<"$a" )
-    #   log $b
-    #}
-    if [ -z "${stringToCypher}" ];then
-	onePadUsage
-	return 1;
-    fi
-
-    random=$RANDOM
-    cyphered=$null
-    while [ -n "${stringToCypher}" ];do
-	dateSeconds=$(date "+%S")
-
-	if [ ${dateSeconds} -le 09 ];then
-	    continue
-	elif [ ${dateSeconds} -ge 20 ];then
-	    dateSeconds=$(( $dateSeconds / 4 ))
-	fi
-	
-	shifts=$(( $RANDOM / 2 % $dateSeconds ))
-	
-	if [[ "${shifts}" < 2 ]];then
-	    continue
-	fi
-	
-	#print one character
-	
-	addCharacter=$( onePadAlphaTest "$( printf "%c\n" "${stringToCypher}" )" )
-	[ -z "${addCharacter}" ] && return;
-	
-	
-	# shifts is number of characters to skip,
-	# we are adding the current location of $addCharacter which a second letter
-	# cypheredTextNum will contain the new character location for $addCharacter
-	
-	cypheredTextNum="$(( $addCharacter + $shifts - 1 ))"
-	
-	cypher+=$( oneNumPadNumToAlpha "${cypheredTextNum}" )
-        
-	cypheredShifts+=$shifts,
-	
-	stringToCypher=${stringToCypher#*?}
-    done
-    
-    log "shifts --- ${cypheredShifts%,*}"
-    #unset -v cypheredShifts
-    log "${cypher}"
-    return 0;
+     
 }
